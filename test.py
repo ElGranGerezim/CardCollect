0ed01bf10a2bbe3defd95ee500f014dbc789c9a7
ElGranGerezim <nazanal@live.com>
2022-09-26 19:32:23 -0600

    Working set statement

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
+print("Starting Credential")
+cred = credentials.Certificate('cardcollect-bbefd-a23e50827e56.json')
+
+print("Initialize App")
+app = firebase_admin.initialize_app(cred)
+
+print("Connect")
 db = firestore.client()
 
 doc_ref = db.collection(u'users').document(u'alovelace')
